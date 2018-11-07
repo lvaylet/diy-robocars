43480c819fc3c0053ff820a617da2c488eafe039
Laurent Vaylet <laurent.vaylet@bruitparif.fr>
2018-11-07 22:45:32 +0100

    specify serial speed

--- uart/main.js
+++ uart/main.js
@@ -1,12 +1,11 @@
-// This example shows the use of UART0 with a loopback. To run this correctly, you
-// need to add a jumper wire between GPIO14 and GPIO15 on the RPI3.
-
 'use strict';
 
 var SerialPort = require('serialport');
 var portName = process.env.SERIAL || '/dev/ttyAMA0';
 console.log(`Connecting to ${portName}`);
-var port = new SerialPort(portName);
+var port = new SerialPort(portName, {
+  baudRate: 57600
+});
 
 port.on('open', function() {
   console.log(`Connected to ${portName}`);
