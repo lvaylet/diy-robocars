41e131b8a0eeb3a981dc65975f5574e107003da1
Laurent Vaylet <laurent.vaylet@renault.com>
2018-11-08 11:10:38 +0100

    display instructions on how to start a minicom session in debug mode

--- uart-python/start.sh
+++ uart-python/start.sh
@@ -2,6 +2,7 @@
 
 if [[ -n "${DEBUG}" ]]; then
   echo "Entering infinite loop..."
+  echo 'Start a minicom session with: minicom -b 57600 -o -D "/dev/ttyACM0"'
   while true; do sleep 1; done
 else
   echo "Starting application..."
