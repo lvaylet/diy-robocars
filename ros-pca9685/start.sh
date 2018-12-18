046724aa2cf5d0ec4f51cff7122144660223a6e3
Laurent Vaylet <laurent.vaylet@bruitparif.fr>
2018-12-18 19:13:03 +0100

    move build logic to Dockerfile

--- ros-pca9685/start.sh
+++ ros-pca9685/start.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
+# Enter debug mode when a DEBUG variable is defined, no matter its actual value
 if [[ -n "${DEBUG}" ]]; then
+
   echo "Entering infinite loop..."
   while true; do sleep 1; done
+
 else
-  # echo "Initializing workspace..."
-  # catkin_init_workspace
 
-  echo "Building package..."
-  catkin_make
-
-  echo "Installing package..."
-  catkin_make install
-  # Note: why not `source install/setup.bash?`
+  echo "Setting up package..."
   source devel/setup.bash
 
   echo "Starting node..."
   rosrun pca9685 pca9685_node
+
 fi
